6b254ccf1d354c1f03c12bdbb2f5636707768e14
Robert Hannemann <robert.hannemann@vipcon.com>
2021-05-26 17:54:30 +0200

    add code to save orders

--- step3/index.js
+++ step3/index.js
@@ -3,11 +3,39 @@ const fs = require('fs')
 const app = express()
 const port = 3000
 const nanoid = require('nanoid')
+const bodyParser = require('body-parser');
 
+// Functions
 function getCatalog() {
     console.log('start to read config from file')
     const rawData = fs.readFileSync('conf/catalog.json')
     return JSON.parse(rawData)
+}
+
+function calcTotalPrice(items) {
+    let totalPrice = 0
+    items.forEach(item => {
+        totalPrice += (item.price * item.quantity)
+    })
+    return totalPrice
+}
+
+function createOrder(deliveryAddress, phoneNumber, items) {
+    //create a new ID first
+    const id = getOderId();
+    const totalPrice = calcTotalPrice(items)
+
+    const newOrder = {
+        id,
+        status: 'new',
+        deliveryAddress,
+        phoneNumber,
+        items,
+        totalPrice
+    }
+    console.log('start to write order to file')
+    fs.writeFileSync(`orders/${id}.json`, JSON.stringify(newOrder, null, 2));
+    return newOrder;
 }
 
 function getOderId() {
@@ -20,12 +48,17 @@ function getOder(id) {
     return JSON.parse(rawData)
 }
 
+//Middleware
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World! ' + getOderId())
 })
 
 app.post('/orders', (req, res) => {
-    res.send('Hello World! ' + getOderId())
+    console.log('bpod', req.body)
+    const order = createOrder(req.body.deliveryAddress, req.body.phoneNumber, req.body.items)
+    res.send(order)
 })
 
 app.get('/orders/:id', (req, res) => {
